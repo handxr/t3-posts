1803d0e1ce9cd317f00c2df47e814d5ee82533ca
Han <hander.juan@gmail.com>
2023-04-30 02:06:28 +0200

    adding progressbar

---
+++ src/components/ui/progress-bar.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: "ease",
+  speed: 500,
+  showSpinner: false,
+});
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+const ProgressBar = () => null;
+
+export default ProgressBar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { type AppType } from "next/app";
+import dynamic from "next/dynamic";
 
 import { api } from "@/utils/api";
+
+const ProgressBar = dynamic(() => import("@/components/ui/progress-bar"), {
+  ssr: false,
+});
 
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ProgressBar />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
